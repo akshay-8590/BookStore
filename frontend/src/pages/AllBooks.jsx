f97445e688d656c72048f5fa117438bf35d562c0
akshay-8590 <163671337+akshay-8590@users.noreply.github.com>
2025-01-13 02:03:29 +0530

    Modified Loader

--- frontend/src/pages/AllBooks.jsx
+++ frontend/src/pages/AllBooks.jsx
@@ -18,7 +18,7 @@ const AllBooks = () => {
     <div className='bg-zinc-900 h-auto px-12 py-8'>
       <h4 className='text-3xl text-yellow-100'>All Books</h4>
         {!Data && (
-          <div className='flex items-center justify-center my-8'>
+          <div className='h-screen flex items-center justify-center my-8'>
              <Loader/>
           </div>
       )} 
